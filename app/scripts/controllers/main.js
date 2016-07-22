14f7dccd9b7072c5265157eaa27287d5664a9690
Sumeer Tuli <sumeertuli7@gmail.com>
2016-07-21 20:49:03 -0700

    gets the data from the url

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,19 @@
  * Controller of the intuitUiiApp
  */
 angular.module('intuitUiiApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', MainController);
+
+MainController.$inject= ['movieSearchService'];
+  function MainController(movieSearchService) {
     var mainVm = this;
     mainVm.searchMovie = function (expr) {
+
+    	movieSearchService.getMovie(expr).then(function (result) {
+	      mainVm.Movies = result;
+	      console.log('mainVm.Movies', mainVm.Movies);
+	    }, function (error) {
+	      console.log(error);
+	    });
     	console.log(expr);
     }
-  });
+  }
---
+++ app/scripts/services/movieSearch.service.js
@@ -0,0 +1,28 @@
+(function() {
+	'use strict';
+
+	angular.module('intuitUiiApp').service('movieSearchService', movieSearchService);
+
+	movieSearchService.$inject = ['$http', '$q'];
+
+	function movieSearchService($http, $q) {
+		var self = this;
+
+		self.getMovie = function (expr) {
+			console.log('in service', expr);
+			// defer object is created when you want to tell the caller of the function that whenever the response comes from 
+			// this call, I promise I will return back to you.
+			var defer = $q.defer();
+
+			$http
+				.get('http://www.omdbapi.com/?t='+expr)
+				.then(function (response) {
+					defer.resolve(response.data);//proimse made is successfull
+				}, function (error) {
+					defer.reject(error.status);//proimse made is unsuccessfull
+				});
+
+				return defer.promise;
+		};
+	}
+})();
